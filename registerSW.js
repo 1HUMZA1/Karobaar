4354d96915f5eede76de9f7627b8f769c66c68a2
Humza <humza@example.com>
2026-08-18 16:41:10 +0530

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Karobaar/sw.js', { scope: '/Karobaar/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
